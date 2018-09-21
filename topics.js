657ab76e0fdb7620fd288ea52a6ae78d3611b05a
Veronica L <verolozada12@gmail.com>
2018-09-21 16:57:20 -0400

    js file renamed

--- topics.js
+++ topics.js
@@ -2,21 +2,34 @@
 // array of topics: sitcoms
 
 let topics = ["Friends" , "The Big Bang Theory", "How I Met Your Mother", "Seinfeld", "The Office", "Will and Grace", "The Fresh Prince of Bel-Air", "Full House", "Two and a Half Men", "Frasier", "30 Rock", "Modern Family"];
+console.log(topics);
 
+$("#topics").append(topics);
 
 function buttons() {
     // empty the div in case the user wants to add a new sitcom to the array
-    $("#topics").empty();
+    $("#titles").empty();
+    
     for (var i = 0; topics.length; i++) {
     const button = $("<button>")
     button.attr("data-name", topics[i])
     button.addClass("buttons")
     button.text(topics[i]);
-    $("#topics").append(button);
+    $("#titles").append(button);
     }
-    console.log("hello");
 };
 
 buttons();
 
-// const APIKey = "SbCSA3fSSOWvUtm8kB82oS0mjyKwICEG";
+
+
+
+// the user can add a new movie 
+// function newSit (){
+// let sitcom = $("#new-movie").val().trim();
+// topics.push(sitcom)
+// }
+
+// const myKey = "SbCSA3fSSOWvUtm8kB82oS0mjyKwICEG";
+
+
