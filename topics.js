0028d26255345f7a19995020ecd5b77e69a0145f
Veronica L <verolozada12@gmail.com>
2018-09-23 18:59:01 -0400

    basic functions added

--- topics.js
+++ topics.js
@@ -4,40 +4,94 @@
 $(document).ready(function () {
 
     let topics = ["Friends", "The Big Bang Theory", "How I Met Your Mother", "Seinfeld", "The Office", "Will and Grace", "The Fresh Prince of Bel-Air", "Full House", "Two and a Half Men", "Frasier", "30 Rock", "Modern Family"];
-    //$("#titles").append(topics);
+
     function buttons() {
         // empty the div in case the user wants to add a new sitcom to the array
         $("#titles").empty();
-        for (var i = 0; i < topics.length; i++) {
+
+        for (let i = 0; i < topics.length; i++) {
             const button = $("<button>")
-            button.attr("data-name", topics[i])
             button.addClass("btn")
+            button.attr("data-name", topics[i])
             button.text(topics[i]);
             $("#titles").append(button);
-            // console.log(topics[i]);
         }
-
     };
-    buttons();
 
     function generator() {
+
         $("#titles").on("click", ".btn", function () {
+            $("#gifDiv").empty();
             let userChoice = $(this).attr("data-name");
             console.log(userChoice);
+            // URL from GIPHY
+            let queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                userChoice + "&api_key=SbCSA3fSSOWvUtm8kB82oS0mjyKwICEG&limit=10";
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                const result = response.data;
+
+                // show the 10 gifs
+                for (let j = 0; j < result.length; j++) {
+                    // new div to hold image
+                    const newDiv = $("<div>")
+                    newDiv.addClass("newDiv");
+
+                    // gif grabbed
+                    let image = $("<img>")
+                    image.attr("src", result[j].images.fixed_width_still.url);
+                    image.attr("data-state", "still")
+                    image.addClass("gif-" + j);
+
+                    // rating 
+                    let rating = $("<h6>")
+                    rating.text("Rating: " + result[j].rating);
+                    rating.addClass("rating");
+
+                    newDiv.append(image);
+                    newDiv.append(rating);
+                    $("#gifDiv").append(newDiv);
+
+                    // animate gifs
+                    function animate() {
+                        $("#gifDiv").on("click", ".gif-" + j, function () {
+                        let state = $(this).attr("data-state");
+                        if (state === "still") {
+                            image.attr("src", result[j].images.fixed_width.url)
+                            image.attr("data-state", "animate");
+                        } else {
+                            image.attr("src",result[j].images.fixed_width_still.url)
+                            image.attr("data-state", "still");
+                        }
+                        });
+                    }
+                    animate();
+
+                }
+
+            });
         })
     }
-    generator();
+
+
 
     // the user can add a new movie 
-    function newSit() {
-        let NewSitcom = $("#new-movie").val().trim();
-        topics.push(sitcom)
-    
+    $("#add-sitcom").on("click", function (event) {
+        //allows to add a new sitcom with the enter key
+        event.preventDefault();
+
+        //grab value from the user
+        var newSitcom = $("#sitcom-input").val().trim();
+        topics.push(newSitcom);
         buttons();
-    }
-    
-    // const myKey = "SbCSA3fSSOWvUtm8kB82oS0mjyKwICEG";
-    
-    
+    })
+
+    buttons();
+    generator();
+
 });
 
