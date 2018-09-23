5c6d7fb585c607b32012ec3b105db71cfe46113c
Veronica L <verolozada12@gmail.com>
2018-09-23 01:12:12 -0400

    buttons functions fixed + new background

--- topics.js
+++ topics.js
@@ -1,35 +1,43 @@
 
 // array of topics: sitcoms
 
-let topics = ["Friends" , "The Big Bang Theory", "How I Met Your Mother", "Seinfeld", "The Office", "Will and Grace", "The Fresh Prince of Bel-Air", "Full House", "Two and a Half Men", "Frasier", "30 Rock", "Modern Family"];
-console.log(topics);
+$(document).ready(function () {
 
-$("#topics").append(topics);
+    let topics = ["Friends", "The Big Bang Theory", "How I Met Your Mother", "Seinfeld", "The Office", "Will and Grace", "The Fresh Prince of Bel-Air", "Full House", "Two and a Half Men", "Frasier", "30 Rock", "Modern Family"];
+    //$("#titles").append(topics);
+    function buttons() {
+        // empty the div in case the user wants to add a new sitcom to the array
+        $("#titles").empty();
+        for (var i = 0; i < topics.length; i++) {
+            const button = $("<button>")
+            button.attr("data-name", topics[i])
+            button.addClass("btn")
+            button.text(topics[i]);
+            $("#titles").append(button);
+            // console.log(topics[i]);
+        }
 
-function buttons() {
-    // empty the div in case the user wants to add a new sitcom to the array
-    $("#titles").empty();
-    
-    for (var i = 0; topics.length; i++) {
-    const button = $("<button>")
-    button.attr("data-name", topics[i])
-    button.addClass("buttons")
-    button.text(topics[i]);
-    $("#titles").append(button);
+    };
+    buttons();
+
+    function generator() {
+        $("#titles").on("click", ".btn", function () {
+            let userChoice = $(this).attr("data-name");
+            console.log(userChoice);
+        })
     }
-};
+    generator();
 
-buttons();
-
-
-
-
-// the user can add a new movie 
-// function newSit (){
-// let sitcom = $("#new-movie").val().trim();
-// topics.push(sitcom)
-// }
-
-// const myKey = "SbCSA3fSSOWvUtm8kB82oS0mjyKwICEG";
-
+    // the user can add a new movie 
+    function newSit() {
+        let NewSitcom = $("#new-movie").val().trim();
+        topics.push(sitcom)
+    
+        buttons();
+    }
+    
+    // const myKey = "SbCSA3fSSOWvUtm8kB82oS0mjyKwICEG";
+    
+    
+});
 
